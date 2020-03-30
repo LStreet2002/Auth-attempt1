3676f42526d4d6b26af88bde27f995dbda6d84f9
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-03-30 14:40:43 +0100

    initialise

---
+++ fireBase.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBLNmQEuwfF-e32W3FOK1TyetTyNyIps_E",
+    authDomain: "canon-1auth.firebaseapp.com",
+    databaseURL: "https://canon-1auth.firebaseio.com",
+    projectId: "canon-1auth",
+    storageBucket: "canon-1auth.appspot.com",
+    messagingSenderId: "986529858863",
+    appId: "1:986529858863:web:c25d0d66546b1ff5fa8dda",
+    measurementId: "G-EQY1XYP30P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
