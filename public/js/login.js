4a91485d6f1941896575246245a9ee8fa0aa99e9
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-04-17 13:24:23 +0100

    login etc flushed out

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,9 @@
         // firebase login
         auth.signInWithEmailAndPassword(email, password).then((cred) => {
             window.location.replace("main.html");
-        });
+        }).catch(error => document.querySelector("#login-password").style.border = "solid 1px red"
+            , document.querySelector("#login-email").style.border = "solid 1px red");
+
     })
 })();
 
@@ -20,13 +22,28 @@ function rese() {
     logi.style.display = "none";
     var hol = document.querySelector(".hol")
     hol.style.display = "grid" // switch to change password
+    var res = document.querySelector("#signup-holder")
+    res.style.display = "none"
 }
 function main() {
     var logi = document.querySelector("#login-holder")
     logi.style.display = "grid";
     var hol = document.querySelector(".hol")
     hol.style.display = "none" // switch to main
+    var res = document.querySelector("#signup-holder")
+    res.style.display = "none"
+    document.querySelector("#send").style.backgroundColor = "#F10F0F"
+    document.querySelector("#send").innerText = "SEND";
+    document.getElementById("reset-password").value = ""
 
+}
+function account() {
+    var logi = document.querySelector("#login-holder")
+    logi.style.display = "none";
+    var hol = document.querySelector(".hol")
+    hol.style.display = "none" // switch to main
+    var res = document.querySelector("#signup-holder")
+    res.style.display = "grid"
 }
 (function () {// reset email
     const reset = document.querySelector(".hol");
@@ -37,9 +54,30 @@ function main() {
 
         //send 
         auth.sendPasswordResetEmail(email).then(function () {
-            alert("E-mail sent");
+            document.querySelector("#send").style.backgroundColor = "#77FF00"
+            document.querySelector("#send").innerText = "SENT";
         }).catch(error => alert(error.message));
     })
 })();
 
+(function () {// signup
+    const signForm = document.querySelector('#signup-holder');
+    signForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // get user info
+        const userName = signForm['signup-username'].value;
+        const email = signForm['signup-email'].value;
+        const password = signForm['signup-password'].value;
+
+        // sign up the user
+        auth.createUserWithEmailAndPassword(email, password).then(function (result) {
+            return result.user.updateProfile({
+                displayName: userName
+            })
+        }).then(cred => {
+            main()
+        });
+    });
+})();
 
